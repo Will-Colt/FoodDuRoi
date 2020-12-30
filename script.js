5f45706e887ec47875987caa40cdac660c7185a4
William Colletin <colletin@hotmail.fr>
2020-12-30 23:56:55 +0100

    forth commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
   $('#menu__button').click(function(){
-    $('.container__menu').css('left', 0);
+    $('.container__menu').css({'left': 0, "opacity": 100});
+    $('#close__button').css("opacity", 100);
+
+  
+  });
+  $('#close__button').click(function(){
+    $('.container__menu').css({'left': '100vw', "opacity": 0});
   });
   });
